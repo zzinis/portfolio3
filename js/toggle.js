04ea67a04bbad67ad069c524dedca76f0e890d5a
zzinis <lizzyin@naver.com>
2021-10-01 21:33:50 +0900

    toggle js 분리

--- js/toggle.js
+++ js/toggle.js
@@ -1,13 +1,21 @@
 const $frame = $("#wrap");
-const $btns = $frame.find("dt");
-const $boxs = $frame.find("dd");
+const $tab_btns = $frame.find("dt");
+const $tab_boxs = $frame.find("dd");
 let speed = 500;
 
-$btns.on("click", function(e){
+$tab_btns.on("click", function(e){
     e.preventDefault();
 
-    let isOn = $(this).hasClass("on");
+    var isOn = $(this).hasClass("on");
 
-    $btns.removeClass("on");
-    $boxs.slideUp(speed);
-})
+    $tab_btns.removeClass("on");
+    $tab_boxs.slideUp(speed);
+
+    if (isOn){
+        $(this).removeClass("on");
+        $(this).next().slideUp(speed);
+    }else{
+        $(this).addClass("on");
+        $(this).next().slideDown(speed);
+    }
+});
